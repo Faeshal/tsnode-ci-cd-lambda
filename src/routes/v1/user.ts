3a24f69e57a930bc68e84ef7e1e3e74dbf599520
faeshal_ <faeshal@faeshals-MacBook-Pro.local>
2023-11-02 21:24:47 +0700

    adjust

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -3,6 +3,6 @@ import * as userController from "../../controllers/user"
 const router = express.Router()
 import { protect, authorize } from "../../middleware/auth";
 
-router.get("/users", protect, userController.getUsers);
+router.get("/users", userController.getUsers);
 
 export default router
