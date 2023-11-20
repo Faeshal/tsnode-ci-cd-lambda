29721445f6dccf0363fcbecddd7a17b085301c1a
faeshal_ <faeshal@faeshals-MacBook-Pro.local>
2023-11-20 15:02:51 +0700

    add endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,4 +11,12 @@ router.get("/", (req, res, next) => {
     .json({ success: true, message: "welcome to the Express API", host: process.env.DB_HOST });
 });
 
+
+router.get("/info", (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, data: { ip: req.ip, path: req.path, timestamp: new Date() } });
+});
+
+
 export default router;
